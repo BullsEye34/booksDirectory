391c652a8a86eaedfd999df0e5ce3940ba392662
Vamshi Prasad <vamvijkum@gmail.com>
2021-06-13 20:13:19 +0530

    Middleware and comments

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 
+// Define App to be an Express APp
 const app=express();
 
+// MiddleWares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+
+// Endpoints
+
+// Serve Home page
 app.get("/",(req,res)=>{
     res.sendFile(process.cwd()+"/pages/index.html");
 });
