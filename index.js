4d75639bb30c6d451d126f19086bd5fded41c3d3
Vamshi Prasad <vamvijkum@gmail.com>
2021-06-13 20:10:34 +0530

    Semi Colons

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const express = require('express')
 const app=express();
 
 app.get("/",(req,res)=>{
-    res.sendFile(process.cwd()+"/pages/index.html")
+    res.sendFile(process.cwd()+"/pages/index.html");
+});
 
-})
 
-
-app.listen(3000,()=>console.log("Listening on : http://127.0.0.1:3000/"))
+app.listen(3000,()=>console.log("Listening on : http://127.0.0.1:3000/"));
