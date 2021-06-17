116a4c7122d93231b7816cc5f5e41dafc06807b6
Vamshi Prasad <vamvijkum@gmail.com>
2021-06-17 19:14:16 +0530

    User Model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    phone:{
+        type: Number,
+        required: true,
+    },
+    img:{
+        type: String,
+        default:"https://static.vecteezy.com/system/resources/previews/001/196/558/original/person-png.png"
+    }
+});
